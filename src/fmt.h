5ee3326667c9c851d20339d542329a3e287e42eb
Riley Cornelius <54592152+RileyCornelius@users.noreply.github.com>
2025-06-14 23:37:33 -0500

    Significantly reduce binary size

--- src/fmt.h
+++ src/fmt.h
@@ -1,6 +1,10 @@
 // Use the header only because compiling the source with the Arduino build system is difficult.
 #define FMT_HEADER_ONLY
 
+// Significantly reduce binary size - https://vitaut.net/posts/2024/binary-size/
+#define FMT_USE_LOCALE 0    // Disable locale support
+#define FMT_BUILTIN_TYPES 0 // Only include formatting types when they are used (increase per call size, but reduces library size)
+
 // Backup conflicting macros
 #pragma push_macro("F")
 #pragma push_macro("B1")
@@ -10,7 +14,7 @@
 #undef F
 
 // Include the library
-#include <fmt/core.h>
+#include <fmt/format.h>
 
 // Restore conflicting macros
 #pragma pop_macro("F")
